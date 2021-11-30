a34e6913a58585f04bd79d264ce0498a835f1b53
vanlociter1996 <vanlociter1996@gmail.com>
2021-11-30 12:24:10 +0700

    ternary search

--- src/GT/Search.java
+++ src/GT/Search.java
@@ -21,9 +21,27 @@ public class Search {
         return binarySearch(a, x, middle + 1, right);
     }
 
+    public static int ternarySearch(int[] a, int x, int left, int right) {
+        int partion = (right - left) / 3;
+        int middle1 = left + partion;
+        int middle2 = middle1 + partion;
+        if(right < left)
+            return -1;
+        if (x == a[middle1])
+            return middle1;
+        if (x == a[middle2])
+            return middle2;
+        if (x < a[middle1])
+            return ternarySearch(a, x, left, middle1 - 1);
+        else if (x > a[middle1] && x < a[middle2])
+            return ternarySearch(a, x, middle1 + 1, middle2 - 1);
+        else
+            return ternarySearch(a, x, middle2 + 1, right);
+    }
+
     public static void main(String[] args) {
         int[] a = {1, 3, 4, 5, 6};
-        int r = binarySearch(a, 6, 0, a.length - 1);
+        int r = ternarySearch(a, 7, 0, a.length - 1);
         System.out.println(r);
     }
 }
